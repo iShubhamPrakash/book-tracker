d27284893da8829288cd29bd7a3bf3552efac00e
i-shubhamprakash <shubham.prakash2308@gmail.com>
2018-10-30 20:05:01 +0530

    Remove hardcoded book details from the book component

--- src/components/ListBooks.js
+++ src/components/ListBooks.js
@@ -14,33 +14,33 @@ class ListBooks extends Component {
         <div>
             <div className="bookshelf">
                 <h2 className="bookshelf-title">Currently Reading</h2>
-                <div className="bookshelf-books">
-                    <ol className="books-grid">
-                        <li>
+                    <div className="bookshelf-books">
+                        <ol className="books-grid">
+                            <li>
 
-                        </li>
-                    </ol>
-                </div>
+                            </li>
+                        </ol>
+                    </div>
             </div>
             <div className="bookshelf">
                 <h2 className="bookshelf-title">Want to Read</h2>
-                <div className="bookshelf-books">
-                <ol className="books-grid">
-                    <li>
+                    <div className="bookshelf-books">
+                        <ol className="books-grid">
+                            <li>
 
-                    </li>
-                </ol>
-                </div>
+                            </li>
+                        </ol>
+                    </div>
             </div>
             <div className="bookshelf">
                 <h2 className="bookshelf-title">Read</h2>
-                <div className="bookshelf-books">
-                <ol className="books-grid">
-                    <li>
+                    <div className="bookshelf-books">
+                        <ol className="books-grid">
+                            <li>
 
-                    </li>
-                </ol>
-                </div>
+                            </li>
+                        </ol>
+                    </div>
             </div>
         </div>
     </div>
