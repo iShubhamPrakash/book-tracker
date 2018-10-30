da2ea4f2dc0791ed2908f82a4373418ff36c6259
i-shubhamprakash <shubham.prakash2308@gmail.com>
2018-10-30 20:18:30 +0530

    Add book array to hold book list fetched by the API call

--- src/App.js
+++ src/App.js
@@ -1,32 +1,30 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 import Footer from './components/Footer'
 import SearchBooks from './components/SearchBooks'
 import ListBooks from './components/ListBooks'
+import Book from './components/Book';
 
 
 class BooksApp extends React.Component {
 
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
+    books:[]
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then(bookList => {
+      this.setState({books:bookList})
+    })
   }
 
   render() {
     return (
       <div className="app">
 
-        <SearchBooks />
-        <ListBooks />
-
-        {/* {this.state.showSearchPage ? (<SearchBooks />) : (<ListBooks/>)} */}
-
+        <ListBooks
+        books={this.state.books}/>
 
         <Footer />
 
