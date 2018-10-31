d81f2d5f4b110a28bc3fdc550792c2a6cabe32c8
i-shubhamprakash <shubham.prakash2308@gmail.com>
2018-11-01 01:42:37 +0530

    Add: value of input changes as typed in the input field

--- src/components/SearchBooks.js
+++ src/components/SearchBooks.js
@@ -4,21 +4,27 @@ import Book from './Book';
 import PropTypes from 'prop-types';
 
 class SearchBooks extends Component {
+
+  state = {
+    query:""
+  }
+
+  updateQuery= (query) => {
+    this.setState({query:query})
+  }
+
   render() {
       return (
           <div className="search-books">
           <div className="search-books-bar">
             <a className="close-search">Close</a>
             <div className="search-books-input-wrapper">
-              {/*
-                NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                You can find these search terms here:
-                https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                you don't find a specific author or title. Every search is limited by search terms.
-              */}
-              <input type="text" placeholder="Search by title or author"/>
+              <input
+              type="text"
+              placeholder="Search by title or author"
+              value={this.state.query}
+              onChange={(event) => this.updateQuery(event.target.value)}
+              />
 
             </div>
           </div>
